13547f247efe4d0a394516de614783fb25f3ec2a
hoyeon <highoption@naver.com>
2023-11-09 18:24:19 +0900

    init project

---
+++ alom/src/main/java/alom/alom/AlomApplication.java
@@ -0,0 +1,13 @@
+package alom.alom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlomApplication.class, args);
+	}
+
+}
---
+++ alom/src/test/java/alom/alom/AlomApplicationTests.java
@@ -0,0 +1,13 @@
+package alom.alom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
